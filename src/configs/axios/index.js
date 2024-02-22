9feec707bbb1437003515f7d2ce8730714203cbb
Alice <90101416+aliceiswondering@users.noreply.github.com>
2024-02-22 22:06:01 +0700

    Update axios index.js

--- src/configs/axios/index.js
+++ src/configs/axios/index.js
@@ -5,7 +5,7 @@ import errorResponseHandler from './errorResponseHandler'
 const instance = axios.create({
   baseURL: `${process.env.REACT_APP_HOST}/api/v1/member`,
 });
-
+instance.defaults.headers.common['ngrok-skip-browser-warning'] = 'headers-server-staycation';
 instance.interceptors.response.use(
   (response) => response,
   errorResponseHandler
