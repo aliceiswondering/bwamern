4657265b02e51b6ae220904d4d652c4c0241da66
Alice <90101416+aliceiswondering@users.noreply.github.com>
2024-02-23 16:47:40 +0700

    Update index.js

--- src/configs/axios/index.js
+++ src/configs/axios/index.js
@@ -6,6 +6,7 @@ const instance = axios.create({
   baseURL: `${process.env.REACT_APP_HOST}/api/v1/member`,
 });
 instance.defaults.headers.common['ngrok-skip-browser-warning'] = 'headers-server-staycation';
+instance.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 instance.interceptors.response.use(
   (response) => response,
   errorResponseHandler
